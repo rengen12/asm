fa4c5d9567d5d70d0ce8c646eb2d29ade82c61ec
Denis BUY <dbuy@e2r7p3.unit.ua>
2018-03-24 16:09:39 +0200

    g

--- core_champs/zork_reaper.s
+++ core_champs/zork_reaper.s
@@ -1,25 +1,10 @@
-.name           "gavgav_in_da_bocal"
-.comment        ""
+.name           "Zork_reaper"
+.comment        "gavgav_in_da_bocal"
 	
 	sti	r1,%:live,%1
 live:	
 	live	%1
 	ld %57672193, r2
-
-#	st r2, 500
-#	st r2, 431
-#	st r2, 362
-#	st r2, 293
-
-#	st r2, 493
-#	st r2, 424
-#	st r2, 355
-#	st r2, 286
-
-#	st r2, 493
-#	st r2, 424
-#	st r2, 355
-#	st r2, 286
 
 	st r2, 493
 	st r2, 424
